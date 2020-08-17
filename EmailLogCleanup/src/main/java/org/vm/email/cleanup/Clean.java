25af82114ed93cbbcad7ec5817a5b28cfe56e28e
smashed_toes <samuel_masto@brown.edu>
2020-08-17 16:51:47 -0400

    Factored out some code to a Writer class, now allows for creation of multiple files from one run.

--- EmailLogCleanup/src/main/java/org/vm/email/cleanup/Clean.java
+++ EmailLogCleanup/src/main/java/org/vm/email/cleanup/Clean.java
@@ -31,8 +31,7 @@ public class Clean implements RequestHandler<Object, String> {
             String offset = System.getenv("OFFSET");
             Timestamp dateX = convertToTimestamp(offset);
             
-            //Retrieve name for CSV file and Number of Emails desired.
-            String filename = System.getenv("LOGNAME");
+            //Retrieve the number of emails to be logged to each created file.
             String emailNum = System.getenv("EMAILNUM");
             int maxEmailNum = Integer.parseInt(emailNum);
             
@@ -51,29 +50,44 @@ public class Clean implements RequestHandler<Object, String> {
             String jdbc = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName+ "?user=" + dbUser + "&password=" + dbPass;
             Connection con = DriverManager.getConnection(jdbc);
             
-            //Creates new csv file to be written to.
-            File file = new File("/tmp/" + filename + ".csv");
-            if (file.createNewFile()) {
-                System.out.println("File Created: " + file.getName());
-                System.out.println("Path: " + file.getAbsolutePath());
-            } else {
-                System.out.println("ERROR: Something wrong with writing file creation");
-            }
-            
             //Setup guid list to loop through.
             List<String> guidList = new ArrayList<>();
             
-            //Sets up the csvWriter and the header for the csv.
-            FileWriter csvWriter = new FileWriter("/tmp/" + filename + ".csv");
-            writeCSVHeader(csvWriter);
+            //Sets up variables used for creating new files.
+            Writer writer = null;
+            FileWriter csvWriter = null;
+            String firstTimestamp = null;
+            S3LogWriter logWriter = null;
+            int fileNumber = 1;
      
             //Loop through all emails from before selected date of a certain type and add their info to a CSV file.
-            String query = "SELECT * FROM mail_log WHERE sent_time < ? AND type != 'custom_email_to_volunteer' ORDER BY sent_time ASC LIMIT ?";
+            String query = "SELECT * FROM mail_log WHERE sent_time < ? AND type != 'custom_email_to_volunteer' ORDER BY sent_time ASC";
             PreparedStatement statement = con.prepareStatement(query);
             statement.setTimestamp(1, dateX);
-            statement.setInt(2, maxEmailNum);
             ResultSet rs = statement.executeQuery();
             while (rs.next()) {
+                //If no file has been created yet, create the first file.
+                if (csvWriter == null) {
+                    firstTimestamp = tsToString(rs.getTimestamp("sent_time"));
+                    writer = new Writer(firstTimestamp + "_" + fileNumber);
+                    csvWriter = writer.getFileWriter();
+                
+                //If a file has already been created but it has recorded the max number of emails, write file
+                //to S3 bucket and create a new file to write to.
+                } else if (maxEmailNum == 0) {
+                    csvWriter.flush();
+                    csvWriter.close();
+                    logWriter = new S3LogWriter(writer.getFile(), bucketName);
+                    logWriter.writeLog();
+                    writer.deleteFile();
+                    fileNumber ++;
+                    writer = new Writer(firstTimestamp + "_" + fileNumber);
+                    csvWriter = writer.getFileWriter();
+                    maxEmailNum = Integer.parseInt(emailNum);
+                }
+                //Decrements the maxEmailNum to track how many emails have been recorded in the current file.
+                maxEmailNum --;
+    
                 List<String> mailInfo = new ArrayList<>();
                 mailInfo.add(Integer.toString(rs.getInt("id")));
                 mailInfo.add(rs.getString("email"));
@@ -83,13 +97,19 @@ public class Clean implements RequestHandler<Object, String> {
                 mailInfo.add(rs.getString("ref2"));
                 mailInfo.add(tsToString(rs.getTimestamp("sent_time")));
                 mailInfo.add(rs.getString("type"));
+                
+                //Query to find all sendgrid events associated with an email.
                 query = "SELECT * FROM sendgrid_event WHERE sendgrid_event.guid = ?";
                 statement = con.prepareStatement(query);
                 String currGuid = rs.getString("guid");
                 guidList.add(currGuid);
                 statement.setString(1, currGuid);
                 ResultSet rs2 = statement.executeQuery();
+                
+                //Boolean created to track whether or not a given email has associated events.
+                boolean events = false;
                 while (rs2.next()) {
+                    events = true;
                     List<String> eventInfo = new ArrayList<>();
                     eventInfo.add(rs2.getString("sg_event_id"));
                     eventInfo.add(Integer.toString(rs2.getInt("asm_group_id")));
@@ -111,82 +131,101 @@ public class Clean implements RequestHandler<Object, String> {
                     eventInfo.add(rs2.getString("url_offset"));
                     eventInfo.add(rs2.getString("useragent"));
                     eventInfo.add(tsToString(rs2.getTimestamp("vm_timestamp")));
+                    
+                    //Writes mail log and sendgrid event info into a new line in the CSV file.
                     csvWriter.append(String.join(",", mailInfo));
                     csvWriter.append(",");
                     csvWriter.append(String.join(",", eventInfo));
                     csvWriter.append("\n");
                 }
+                //If no events associated with a given email, it adds it to the CSV file with no events.
+                if (!events) {
+                    csvWriter.append(String.join(",", mailInfo));
+                    csvWriter.append(",");
+                    csvWriter.append("No events associated with this email");
+                    csvWriter.append("\n");
+                }
                 rs2.close();
             }
             rs.close();
-            csvWriter.flush();
-            csvWriter.close();
-            
-            //Creates an instance of the S3LogWriterClass and writes the file to the s3
-            S3LogWriter logWriter = new S3LogWriter(file, bucketName);
-            logWriter.writeLog();
-    
-            //Allows for deletions to occur regardless of references between tables.
-            Statement stat = con.createStatement();
-            stat.execute("SET FOREIGN_KEY_CHECKS=0");
-    
-            //Deletes recorded logs using a SQL array of the guids.
-            String update = "DELETE FROM sendgrid_event WHERE guid IN (?)";
-            String sqlIN = guidList.stream()
-                               .map(String::valueOf)
-                               .collect(Collectors.joining("','", "('", "')"));
-            update = update.replace("(?)", sqlIN);
-            statement = con.prepareStatement(update);
-            int numSendgrid = statement.executeUpdate();
-    
-            update = "DELETE FROM mail_log WHERE guid IN (?)";
-            update = update.replace("(?)", sqlIN);
-            statement = con.prepareStatement(update);
-            int numMailLog = statement.executeUpdate();
-    
-            //Uses a a select statement to see if there are any of info of type custom_email_to_volunteer.
-            List<String> oppList = new ArrayList<>();
-            query = "SELECT guid FROM mail_log WHERE sent_time < ? AND type = 'custom_email_to_volunteer'";
-            statement = con.prepareStatement(query);
-            statement.setTimestamp(1, dateX);
-            ResultSet rs3 = statement.executeQuery();
-            while (rs3.next()) {
-                oppList.add(rs3.getString("guid"));
-            }
-            rs3.close();
-    
-            //If there are inputs of custom_email_to_volunteer, delete them.
-            if (!oppList.isEmpty()) {
-                update = "DELETE FROM mail_log WHERE sent_time < ? AND type = 'custom_email_to_volunteer'";
-                statement = con.prepareStatement(update);
-                statement.setTimestamp(1, dateX);
-                int mailLogAdd = statement.executeUpdate();
         
-                update = "DELETE FROM sendgrid_event WHERE guid IN (?)";
-                sqlIN = oppList.stream()
-                            .map(String::valueOf)
-                            .collect(Collectors.joining("','", "('", "')"));
+            //If CSV writer is not null, that means that entries were found and recorded and are ready to be deleted.
+            if (csvWriter != null) {
+                csvWriter.flush();
+                csvWriter.close();
+    
+                //Writes the last log to S3 bucket.
+                assert writer != null;
+                logWriter = new S3LogWriter(writer.getFile(), bucketName);
+                logWriter.writeLog();
+                
+                //Allows for deletions to occur regardless of references between tables.
+                Statement stat = con.createStatement();
+                stat.execute("SET FOREIGN_KEY_CHECKS=0");
+    
+                //Deletes recorded logs using a SQL array of the guids.
+                String update = "DELETE FROM sendgrid_event WHERE guid IN (?)";
+                String sqlIN = guidList.stream()
+                                   .map(String::valueOf)
+                                   .collect(Collectors.joining("','", "('", "')"));
                 update = update.replace("(?)", sqlIN);
                 statement = con.prepareStatement(update);
-                int sendGridAdd = statement.executeUpdate();
+                int numSendgrid = statement.executeUpdate();
+    
+                update = "DELETE FROM mail_log WHERE guid IN (?)";
+                update = update.replace("(?)", sqlIN);
+                statement = con.prepareStatement(update);
+                int numMailLog = statement.executeUpdate();
+    
+                //Uses a a select statement to see if there are any of info of type custom_email_to_volunteer.
+                List<String> oppList = new ArrayList<>();
+                query = "SELECT guid FROM mail_log WHERE sent_time < ? AND type = 'custom_email_to_volunteer'";
+                statement = con.prepareStatement(query);
+                statement.setTimestamp(1, dateX);
+                ResultSet rs3 = statement.executeQuery();
+                while (rs3.next()) {
+                    oppList.add(rs3.getString("guid"));
+                }
+                rs3.close();
+                //If there are inputs of custom_email_to_volunteer, delete them.
+                if (!oppList.isEmpty()) {
+                    update = "DELETE FROM mail_log WHERE sent_time < ? AND type = 'custom_email_to_volunteer'";
+                    statement = con.prepareStatement(update);
+                    statement.setTimestamp(1, dateX);
+                    int mailLogAdd = statement.executeUpdate();
         
-                numMailLog = numMailLog + mailLogAdd;
-                numSendgrid = numSendgrid + sendGridAdd;
+                    update = "DELETE FROM sendgrid_event WHERE guid IN (?)";
+                    sqlIN = oppList.stream()
+                                .map(String::valueOf)
+                                .collect(Collectors.joining("','", "('", "')"));
+                    update = update.replace("(?)", sqlIN);
+                    statement = con.prepareStatement(update);
+                    int sendGridAdd = statement.executeUpdate();
+        
+                    numMailLog = numMailLog + mailLogAdd;
+                    numSendgrid = numSendgrid + sendGridAdd;
+                }
+    
+                //Closes connections and statements.
+                stat.execute("SET FOREIGN_KEY_CHECKS=1");
+                statement.close();
+                stat.close();
+                con.close();
+    
+                System.out.println("SUCCESS: " + numSendgrid + " rows deleted from sendgrid_event table and " + numMailLog
+                                       + " rows deleted from mail_log table. Deleted information has been recorded in "
+                                       + bucketName + " in files titled emailEventStore" + firstTimestamp + ".csv");
+    
+                return "SUCCESS: " + numSendgrid + " rows deleted from sendgrid_event table and " + numMailLog
+                           + " rows deleted from mail_log table. Deleted information has been recorded in "
+                           + bucketName + " in files titled emailEventStore" + firstTimestamp + ".csv";
+                
+            //If the CSVwriter was null, that means no data fit the criteria to be deleted.
+            } else {
+                System.out.println("No data in the specified time frame. Nothing deleted or recorded from database");
+                return "No data in the specified time frame. Nothing deleted or recorded from database";
             }
             
-            //Closes connections and statements.
-            stat.execute("SET FOREIGN_KEY_CHECKS=1");
-            statement.close();
-            stat.close();
-            con.close();
-            
-            System.out.println("SUCCESS: " + numSendgrid + " rows deleted from sendgrid_event table and " + numMailLog
-                                   + " rows deleted from mail_log table. Deleted information has been recorded in "
-                                   + bucketName + " in file " + filename + ".csv");
-            
-            return "SUCCESS: " + numSendgrid + " rows deleted from sendgrid_event table and " + numMailLog
-                       + " rows deleted from mail_log table. Deleted information has been recorded in "
-                       + bucketName + " in file " + filename + ".csv";
         } catch (ClassNotFoundException | SQLException | ParseException | IOException e) {
             e.printStackTrace();
             return "ERROR: Database connection error or parsing error";
@@ -225,67 +264,4 @@ public class Clean implements RequestHandler<Object, String> {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
     }
     
-    /**
-     * A helper method that prepares the CSV file header.
-     * @param csvWriter with nothing in it.
-     * @throws IOException if I/O error occurs.
-     */
-    private void writeCSVHeader(FileWriter csvWriter) throws IOException {
-        csvWriter.append("ml_id");
-        csvWriter.append(",");
-        csvWriter.append("ml_email");
-        csvWriter.append(",");
-        csvWriter.append("ml_guid");
-        csvWriter.append(",");
-        csvWriter.append("ml_host");
-        csvWriter.append(",");
-        csvWriter.append("ml_ref1");
-        csvWriter.append(",");
-        csvWriter.append("ml_ref2");
-        csvWriter.append(",");
-        csvWriter.append("ml_sent_time");
-        csvWriter.append(",");
-        csvWriter.append("ml_type");
-        csvWriter.append(",");
-        csvWriter.append("se_sg_event_id");
-        csvWriter.append(",");
-        csvWriter.append("se_asm_group_id");
-        csvWriter.append(",");
-        csvWriter.append("se_attempt");
-        csvWriter.append(",");
-        csvWriter.append("se_category");
-        csvWriter.append(",");
-        csvWriter.append("se_email");
-        csvWriter.append(",");
-        csvWriter.append("se_event");
-        csvWriter.append(",");
-        csvWriter.append("se_ip");
-        csvWriter.append(",");
-        csvWriter.append("se_reason");
-        csvWriter.append(",");
-        csvWriter.append("se_response");
-        csvWriter.append(",");
-        csvWriter.append("se_sg_message_id");
-        csvWriter.append(",");
-        csvWriter.append("se_smtp_id");
-        csvWriter.append(",");
-        csvWriter.append("se_status");
-        csvWriter.append(",");
-        csvWriter.append("se_timestamp");
-        csvWriter.append(",");
-        csvWriter.append("se_tls");
-        csvWriter.append(",");
-        csvWriter.append("se_type");
-        csvWriter.append(",");
-        csvWriter.append("se_unsubscribe_url");
-        csvWriter.append(",");
-        csvWriter.append("se_url");
-        csvWriter.append(",");
-        csvWriter.append("se_url_offset");
-        csvWriter.append(",");
-        csvWriter.append("se_useragent");
-        csvWriter.append(",");
-        csvWriter.append("se_vm_timestamp");
-        csvWriter.append("\n");
-    }
 }
